5b501258346cbdee5165aea70d24f924ffc0c108
elvismalsa <elvisotienomboya@gmail.com>
2023-07-17 23:25:15 +0300

    the main header file

--- shell.h
+++ shell.h
@@ -20,6 +20,12 @@ int _atoi(char *str);
 char *_getline(char **cmd, FILE *stream);
 void exit_shell(int status);
 void clear_scrn();
+char *_getenv(const char *name);
+void printEnv(void);
+int _unsetenv(char *var);
+int _setenv(const char *name, const char *value, int overwrite);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int cmd_execution(char *cmd);
 int main(void);
 
 #endif
