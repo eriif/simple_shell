780eb64b7c5a452d9b9511d38285d6796c654a9e
elvismalsa <elvisotienomboya@gmail.com>
2023-07-17 20:46:04 +0300

    a getline func

--- _getline.c
+++ _getline.c
@@ -11,53 +11,63 @@
  */
 char *_getline(char **cmd, FILE *stream)
 {
-	static char buffer[BUFFER_SIZE];
-	static int index = 0;
-	static int bytesRead = 0;
-	char c;
-	int Lsize = 0;
+    static char buffer[BUFFER_SIZE];
+    static int index = 0;
+    static int bytesRead = 0;
+    static int currentBuff = BUFFER_SIZE;
+    char c;
+    char *newCmd;
+    int Lsize = 0;
 
-	if (*cmd == NULL)
-		*cmd = malloc(sizeof(char));
+    if (*cmd == NULL)
+    {
+        *cmd = malloc(currentBuff * sizeof(char));
 
-	if (*cmd == NULL)
-	{
-		perror("Memory allocation error");
-		exit(EXIT_FAILURE);
-	}
+        if (*cmd == NULL)
+        {
+            perror("Memory allocation error");
+            exit(EXIT_FAILURE);
+        }
+    }
 
-	while (1)
-	{
-		if (index == bytesRead)
-		{
-			bytesRead = read(fileno(stream), buffer, BUFFER_SIZE);
-			if (bytesRead == -1)
-			{
-				perror("Error from read");
-				exit(EXIT_FAILURE);
-			}
-			if (bytesRead == 0)
-				break;
-			index = 0;
-		}
+    while (1)
+    {
+        if (index == bytesRead)
+        {
+            bytesRead = read(fileno(stream), buffer, BUFFER_SIZE);
+            if (bytesRead == -1)
+            {
+                perror("Error from read");
+                exit(EXIT_FAILURE);
+            }
+            if (bytesRead == 0)
+                break;
+            index = 0;
+        }
 
-		c = buffer[index++];
-		if (c == '\n')
-		{
-			(*cmd)[Lsize] = '\0';
-			break;
-		}
+        c = buffer[index++];
+        if (c == '\n')
+        {
+            (*cmd)[Lsize] = '\0';
+            break;
+        }
 
-		*cmd = realloc(*cmd, Lsize + 2);
-		if (*cmd == NULL)
-		{
-			perror("Memory allocation error");
-			exit(EXIT_FAILURE);
-		}
+        if (Lsize + 1 == currentBuff)
+        {
+            currentBuff *= 2;
+            newCmd = malloc(currentBuff * sizeof(char));
+            if (newCmd == NULL)
+            {
+                perror("Memory allocation error");
+                exit(EXIT_FAILURE);
+            }
+            memcpy(newCmd, *cmd, Lsize);
+            free(*cmd);
+            *cmd = newCmd;
+        }
+        (*cmd)[Lsize] = c;
+        Lsize++;
+    }
 
-		(*cmd)[Lsize] = c;
-		Lsize++;
-	}
-
-	return (*cmd);
+    return (*cmd);
 }
