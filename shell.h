f4d96dec34b7b27d217539ab3d5798f023f15e2b
elvismalsa <elvisotienomboya@gmail.com>
2023-07-19 00:39:22 +0300

    main header file

--- shell.h
+++ shell.h
@@ -1,7 +1,7 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define BUFFER_SIZE 1024
+/*#define BUFFER_SIZE 1024*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,6 +13,8 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdarg.h>
+
+extern char **environ;
 
 int _strncmp(const char *str1, const char *str2, size_t n);
 int _strcmp(char *s1, char *s2);
@@ -26,6 +28,7 @@ int _unsetenv(char *var);
 int _setenv(const char *name, const char *value, int overwrite);
 char *_memcpy(char *dest, char *src, unsigned int n);
 int cmd_execution(char *cmd);
+char *_strcat(char *dest, const char *src);
 int main(void);
 
 #endif
