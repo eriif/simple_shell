765dc86328a866fc6e98c5f2d430cda50ff6d8f7
elvismalsa <elvisotienomboya@gmail.com>
2023-07-18 23:26:19 +0300

    functions used

--- myfunc.c
+++ myfunc.c
@@ -75,3 +75,53 @@ int _atoi(char *str)
 
 	return (sign * conv);
 }
+/**
+ * _memcpy - a function that copies memory area
+ * @dest: The memory area to be copied to
+ * @src: The memory area to be copied from
+ * @n: The number of bytes
+ *
+ * Description: The function copies n bytes
+ * from memory area src to memory area dest
+ * Return: a pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a = 0;
+
+	while (a < n)
+	{
+		(dest[a] = src[a]);
+		a++;
+	}
+	return (dest);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
