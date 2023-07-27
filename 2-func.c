a3793c879f8a65256bd78d8ad75bbe611ee64363
elvismalsa <elvisotienomboya@gmail.com>
2023-07-27 09:26:08 +0300

    update

--- 2-func.c
+++ 2-func.c
@@ -1,6 +1,64 @@
 #include "shell.h"
 
 /**
+ * _strspn - The entry point
+ * @s: consists entirely of bytes in accept
+ * @accept: the initial segment of s
+ *
+ * Description: the function that gets the length of a prefix substring
+ * Return: usually 0
+ */
+unsigned int _strspn(char *s, const char *accept)
+{
+	unsigned int y = 0;
+	int z = 0;
+	int accept_len = 0;
+
+	while (accept[accept_len])
+	{
+		accept_len++;
+	}
+	while (*s)
+	{
+		z = 0;
+		while (accept['r'])
+		{
+			if (*s == accept[z])
+			{
+				y++;
+				break;
+			}
+			else if (z == accept_len - 1)
+			{
+				return (y);
+			}
+			z++;
+		}
+		s++;
+	}
+	return (y);
+}
+
+/**
+ * _strchr - The function that locates a character in a string.
+ * @s: is a string
+ * @c: the character to be allocated by a pointer on its
+ * first occurrence
+ *
+ * Return: a pointer to the first occurrence of the character c in the string s
+ * or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+do {
+if (*s == c)
+return (s);
+} while (*s++ != '\0');
+
+return (NULL);
+}
+
+/**
 * interactive - returns true if shell is interactive mode
 * @info: struct address
 * Return: 1 if interactive mode, 0 otherwise
@@ -37,12 +95,6 @@ return (1);
 else
 return (0);
 }
-
-/**
-* _atoi - converts a string to an integer
-* @s: the string to be converted
-* Return: 0 if no numbers in string, converted number otherwise
-**/
 
 int _atoi(char *s)
 {
